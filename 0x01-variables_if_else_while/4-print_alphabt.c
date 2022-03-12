02de0a276df6fd0734c6dda9e263818f771d5a45
pishkim <kimanipeter907@gmail.com>
2022-03-12 12:46:04 -0800

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <ctype.h>
+
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+
+int c = 'A';
+
+while (c <= 'Z')
+{
+if (c != 'E' && c != 'Q')
+{
+putchar(tolower(c));
+}
+c++;
+}
+
+putchar('\n');
+
+return (0);
+}
+
